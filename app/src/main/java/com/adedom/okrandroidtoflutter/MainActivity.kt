65d47de3ddfc211ab5d2539571e98f5548d6d2bf
pathiphon <pathiphon.ja@chococrm.com>
2022-07-19 12:21:16 +0700

    Setup flutter module

--- app/src/main/java/com/adedom/okrandroidtoflutter/MainActivity.kt
+++ app/src/main/java/com/adedom/okrandroidtoflutter/MainActivity.kt
@@ -2,10 +2,16 @@ package com.adedom.okrandroidtoflutter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tvHelloWorld).setOnClickListener {
+            startActivity(FlutterActivity.createDefaultIntent(this))
+        }
     }
 }
